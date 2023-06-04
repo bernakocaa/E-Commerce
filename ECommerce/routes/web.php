f7f5d2fe0188740ad677162f3225debe58ba0615
bernakocaa <bernakoca1907@gmail.com>
2023-06-04 23:46:38 +0300

    first commit

---
+++ ECommerce/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\PageHomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [PageHomeController::class,'anasayfa'])->name('anasayfa');
