cdaddfe95abd9fa1e5b5d543f2406cbe379cd5a9
bernakocaa <bernakoca1907@gmail.com>
2023-06-04 23:46:38 +0300

    first commit

---
+++ ECommerce/resources/views/frontend/inc/footer.blade.php
@@ -0,0 +1,21 @@
+      <!-- footer section start -->
+      <div class="footer_section layout_padding">
+        <div class="container">
+           <div class="footer_logo"><a href="index.html"><img src="images/footer-logo.png"></a></div>
+           <div class="input_bt">
+              <input type="text" class="mail_bt" placeholder="Mail Adresi" name="Your Email">
+              <span class="subscribe_bt" id="basic-addon2"><a href="#">Üye Ol</a></span>
+           </div>
+           <div class="footer_menu">
+              <ul>
+                 <li><a href="#">Çok Satanlar</a></li>
+                 <li><a href="#">Yeni Gelenler</a></li>
+                 <li><a href="#">Hediye Fikirleri</a></li>
+                 <li><a href="#">Günün Teklifleri</a></li>
+                 <li><a href="#">Müşteri Hizmetleri</a></li>
+              </ul>
+           </div>
+           <div class="location_main">İletişim Numarası : <a href="#">+0 212 548 2365</a></div>
+        </div>
+     </div>
+     <!-- footer section end -->
